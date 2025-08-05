370b8284300c3e72f511c55e63ebccc7889b4e16
EThys <ethbergmuzola500@gmail.com>
2025-08-05 18:30:00 +0100

    feat(hero): add new responsive hero component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<!-- eslint-disable @typescript-eslint/ban-ts-comment -->
+<script lang="ts" setup>
+//@ts-ignore
+import HeroComponent from '@/components/hero/HeroComponent.vue'
+// //@ts-ignore
+// import FooterComponent from '@/components/footer/FooterComponent.vue'
+// //@ts-ignore
+// import ServiceComponent from '@/components/services/ServiceComponent.vue'
 </script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <HeroComponent />
+  <!-- <ServiceComponent />
+  <FooterComponent /> -->
 </template>
+<style></style>
