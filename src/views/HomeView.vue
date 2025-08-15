bce5adca3c335c79356fa35b5a1ec842d53760f0
EThys <ethbergmuzola500@gmail.com>
2025-08-15 17:06:30 +0100

    Refactor:Integrate footer component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,13 +2,26 @@
 <script lang="ts" setup>
 //@ts-ignore
 import HeroComponent from '@/components/hero/HeroComponent.vue'
-// //@ts-ignore
-// import FooterComponent from '@/components/footer/FooterComponent.vue'
+//@ts-ignore
+import FeaturedJobsComponent from '@/components/jobs/FeaturedJobsComponent.vue'
+//@ts-ignore
+import AboutComponent from '@/components/about/AboutComponent.vue'
+//@ts-ignore
+import EntrepriseRecruteComponent from '@/components/jobs/EntrepriseRecruteComponent.vue'
+//@ts-ignore
+import TestimonialComponent from '@/components/testimonial/TestimonialComponent.vue'
+//@ts-ignore
+import FooterComponent from '@/components/footer/FooterComponent.vue'
 // //@ts-ignore
 // import ServiceComponent from '@/components/services/ServiceComponent.vue'
 </script>
 <template>
   <HeroComponent />
+  <FeaturedJobsComponent />
+  <AboutComponent />
+  <EntrepriseRecruteComponent />
+  <FooterComponent />
+  <!-- <TestimonialComponent /> -->
   <!-- <ServiceComponent />
   <FooterComponent /> -->
 </template>
