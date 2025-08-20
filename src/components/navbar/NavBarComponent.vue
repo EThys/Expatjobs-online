73ff185b816bea42f7669aac48b47733d7ea355e
Ethys <ethbergmuzola500@gmail.com>
2025-08-20 13:20:31 +0100

    Refactor:add new link

--- src/components/navbar/NavBarComponent.vue
+++ src/components/navbar/NavBarComponent.vue
@@ -178,7 +178,7 @@ onUnmounted(() => {
                 @mouseleave="closeDropdown"
               >
                 <router-link
-                  to="/jobs"
+                  to="/alljobs"
                   class="flex items-center px-4 py-3 text-gray-700 hover:bg-emerald-50 hover:text-emerald-600 transition-colors group"
                   @click="closeAllDropdowns"
                 >
@@ -195,7 +195,7 @@ onUnmounted(() => {
                       d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                     />
                   </svg>
-                  Browse All Jobs
+                  All Jobs
                 </router-link>
                 <router-link
                   to="/jobs/remote"
@@ -241,7 +241,7 @@ onUnmounted(() => {
             </div>
 
             <router-link
-              to="/post-job"
+              to="/postjob"
               class="relative px-4 py-3 text-gray-700 hover:text-emerald-600 font-medium transition-colors group"
               active-class="text-emerald-600"
               @click="closeAllDropdowns"
@@ -406,11 +406,11 @@ onUnmounted(() => {
             >
               <div v-show="activeDropdown === 'mobile-jobs'" class="pl-12 space-y-1">
                 <router-link
-                  to="/jobs"
+                  to="/alljobs"
                   class="block py-2.5 px-4 rounded-lg hover:bg-emerald-50 text-gray-700 text-sm transition-colors"
                   @click="closeAllDropdowns"
                 >
-                  Browse All Jobs
+                  All Jobs
                 </router-link>
                 <router-link
                   to="/jobs/remote"
