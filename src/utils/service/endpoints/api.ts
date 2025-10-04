7c8b8bb824a18b10886fdfa4ce84779df7d3a03f
Ethys <ethbergmuzola500@gmail.com>
2025-10-04 12:19:57 +0100

    add nwe enpointd

--- src/utils/service/endpoints/api.ts
+++ src/utils/service/endpoints/api.ts
@@ -1,9 +1,32 @@
 export const ApiRoutes = {
   //route for authentification
   login: 'login',
-  register: 'register',
+  register: 'users/sign-up/',
   logout: 'logout',
   changePassword: 'changePassword',
+
+  //route for jobSkills
+
+  getAlljobSkills: 'job-skills',
+  updatejobSkills:'job-skills',
+  addjobSkills:'job-skills',
+  showjobSkills:'job-skills/{id}',
+  deletejobSkills:'job-skills/{id}',
+
+  //route for jobOffers
+
+  getAlljobOffers: 'job-offers',
+  updatejobOffers:'job-offers',
+  addjobOffers:'job-offers',
+  showjobOffers:'job-offers/{id}',
+  deletejobOffers:'job-offers/{id}',
+
+  //route for compagy
+
+
+
+
+  
 
   storePlan: 'store/plan',
   AllPlan: 'plans',
