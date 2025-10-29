a95bbbe04ac026aaec636983fcebd41e7a75b382
Ethys <ethbergmuzola500@gmail.com>
2025-10-29 17:42:25 +0100

    Refactor: Correction bug

--- src/utils/interface/user/ICompagny.ts
+++ src/utils/interface/user/ICompagny.ts
@@ -1,7 +1,7 @@
 export interface ICompagny{
-  userId: Number,
-  name: String,
-  description: String,
-  location: String,
-  webSiteUrl: String
+  userId?: number | null     
+  description?: string  
+  name: string,
+  location: string,
+  webSiteUrl: string
 }
