76f183d308e670990c2ff6343a317590a067ac2a
Ethys <ethbergmuzola500@gmail.com>
2025-12-20 07:42:18 +0100

    chore(main): update main entry file

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,18 @@ import App from './App.vue'
 import router from '@/router'
 import i18n from '@/i18n'
 
+import ToastPlugin from 'vue-toast-notification';
+import 'vue-toast-notification/dist/theme-sugar.css';
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(i18n)
+app.use(ToastPlugin, {
+    position: 'top-right',
+    duration: 4000,
+    dismissible: true
+})
 
 app.mount('#app')
